89e6660518472a951016672ec0507a7275fad4ea
Evgeniy <prosperodesu@live.com>
2017-02-17 02:12:47 +0200

    parse from api

--- src/Parser.py
+++ src/Parser.py
@@ -1,6 +1,8 @@
 import urllib, json
 from config.api import ApiConfig
 from MongoDB import Database
+import datetime
+import sys
 '''
 Component for parse meetmix api and get events data
 '''
@@ -20,17 +22,34 @@ class Parse:
 
 	def get_parse_urls(self):
 		base_url = self.api_config.param('meetmix_api_url')
+		cities_ids = self.api_config.param('active_cities')
+		date = datetime.datetime.today().strftime('%Y-%m-%d')
 		urls = []
-		for i in range(1, 1000):
-			url = base_url + '/event/' + str(i)
+		for city_id in cities_ids:
+			url = base_url + '/events/' + str(city_id) + '/' + date
 			urls.append(url)
-		return urls	
+		return urls
 
 	def proccess_urls(self):
+		total = 0;
 		urls = self.get_parse_urls()
 		for url in urls:
+			print 'Parse start [' + datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S') + ']'
+			print 'Url: ' + url
 			data = self.json_request(url)
-			print data['id']
+			counter = 0
+			one_total = len(data)
+			for item in data:
+				collection = self.db.src_data
+				if not collection.find_one({"event_id": item['id']}):
+					item = {'event_id': item['id'], 'description': item['description']}
+					collection.insert_one(item)
+					counter = counter + 1
+					total = total + 1;
+				else:
+					one_total = one_total - 1
+				print 'Items ' + str(counter) + '/' + str(one_total)		
+		print 'Finished. Total items write: ' + str(total)		
 
 			
 
--- src/config/api.py
+++ src/config/api.py
@@ -4,5 +4,6 @@ class ApiConfig(Config):
 
 	def __init__(self):
 		self.data = {
-			'meetmix_api_url': 'http://api.meetmix.ru'
+			'meetmix_api_url': 'http://api.meetmix.ru',
+			'active_cities': [314, 282, 652, 650]
 		}
